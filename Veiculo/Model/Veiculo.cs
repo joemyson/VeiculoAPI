e4b845444eb1483a68d4ec48b12440fdaad64e40
joemyson <joemyson1402@gmail.com>
2022-09-25 14:30:26 -0300

    tornando campo obrigatorio

--- Veiculo/Model/Veiculo.cs
+++ Veiculo/Model/Veiculo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,13 @@ namespace VeiculoAPI.Model
 {
     public class Veiculo
     {
-        public int Id { get; set; }
+        [Required (ErrorMessage ="Campo  Obrigatorio")]
         public string Marca { get; set; }
+        [Required(ErrorMessage = "Campo  Obrigatorio")]
         public string Cor { get; set; }
+        [Required(ErrorMessage = "Campo  Obrigatorio")]
         public string Placa { get; set; }
+        [Required(ErrorMessage = "Campo  Obrigatorio")]
         public string Tipo { get; set; }
 
        
