d07ac0f6cc7d0acac9336bb50a75e29a8bd44f0c
joemyson <joemyson1402@gmail.com>
2022-09-25 13:05:04 -0300

    iniciando projeto de sistema de locação de veiculo

---
+++ Veiculo/Controllers/VeiculoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VeiculoAPI.Model;
+
+namespace VeiculoAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VeiculoController : ControllerBase
+    {
+        private static List<Veiculo> veiculos = new List<Veiculo>();
+
+        [HttpPost]
+        public void AdicionaVeiculo([FromBody]  Veiculo veiculo)
+        {
+            veiculos.Add(veiculo);
+            Console.WriteLine(veiculo.Marca);
+        }
+    }
+}
---
+++ Veiculo/Model/Veiculo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VeiculoAPI.Model
+{
+    public class Veiculo
+    {
+        public int Id { get; set; }
+        public string Marca { get; set; }
+        public string Cor { get; set; }
+        public string Placa { get; set; }
+        public string Tipo { get; set; }
+
+       
+    }
+}
